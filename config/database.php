b29f4786b9771536777fb0de72fc7601c3f2652f
Marcell151 <322310015@student.machung.ac.id>
2026-04-14 14:58:11 +0700

    V1

--- config/database.php
+++ config/database.php
@@ -1,23 +1,23 @@
 <?php
 // config/database.php
 
-$host = '127.0.0.1';
-$port = '3307';
-$db   = 'kms_bsoal';
+$host = 'localhost';
+$port = '3306';
+$db = 'kms_bsoal';
 $user = 'root'; // Standard XAMPP user
 $pass = '';     // Standard XAMPP password
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+     PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
 try {
      $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+     throw new \PDOException($e->getMessage(), (int) $e->getCode());
 }
 ?>
